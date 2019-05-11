2d1806fa315e4ead96c5b86a953b1a10ed9faeba
zzj <zhangzjzzj@foxmail.com>
2019-05-11 15:34:47 +0800

    日志学习第一次提交

---
+++ src/main/java/com/itcast/logging/Demo01.java
@@ -0,0 +1,19 @@
+package com.itcast.logging;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Demo01 {
+	private static Logger logger = Logger.getLogger(Demo01.class);  
+    public static void main(String[] args) { 
+//    	BasicConfigurator.configure();
+//    	PropertyConfigurator.configure("log4j.properties");
+        // 记录debug级别的信息 
+        logger.debug("This is debug message.");  
+        // 记录info级别的信息  
+        logger.info("This is info message.");  
+        // 记录error级别的信息  
+        logger.error("This is error message.");  
+    }  
+}
